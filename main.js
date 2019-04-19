13dde98400f11dcde2bee49f9c717c4a7cfbdd03
ruslanashaari <ruslan.ashaari@gmail.com>
2019-04-20 00:59:36 +0800

    checking if chain is valid through hash calculation and comparing with previous hash

--- main.js
+++ main.js
@@ -32,10 +32,34 @@ class Blockchain{
 		newBlock.hash = newBlock.calculateHash();
 		this.chain.push(newBlock);
 	}
+
+	isChainValid(){
+		for(let i=1; i < this.chain.length; i++){
+			const currentBlock = this.chain[i];
+			const previousBlock = this.chain[i - 1];
+
+			if(currentBlock.hash != currentBlock.calculateHash()){
+				return false;
+			}
+
+			if(currentBlock.previousHash !== previousBlock.hash){
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
 
 let lanCoin = new Blockchain();
 lanCoin.addBlock(new Block(1, "10/07/2017", { amount: 4 }));
 lanCoin.addBlock(new Block(2, "12/07/2017", { amount: 10 }));
 
-console.log(JSON.stringify(lanCoin, null, 4));
+console.log('Is blockchain valid? ' + lanCoin.isChainValid());
+
+lanCoin.chain[1].data = { amount: 100 };
+lanCoin.chain[1].hash = lanCoin.chain[1].calculateHash();
+
+console.log('Is blockchain valid? ' + lanCoin.isChainValid());
+
+// console.log(JSON.stringify(lanCoin, null, 4));
